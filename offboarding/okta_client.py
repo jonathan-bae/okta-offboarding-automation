b0fbf56bc12e887dc263e94191f2ab165614ab19
jonathan-bae <271075851+jonathan-bae@users.noreply.github.com>
2026-09-24 14:00:14 -0700

    Add get_user and list_groups to OktaClient

--- offboarding/okta_client.py
+++ offboarding/okta_client.py
@@ -7,26 +7,32 @@ TIMEOUT_SECONDS = 10
 
 
 class OktaClient:
+    """Class used to talk to the OKTA API"""
     def __init__(self, config):
         self.base_url = config.okta_domain
         self.session = requests.Session()
         self.session.headers.update({
             "Accept": "application/json",
             "Authorization": f"SSWS {config.okta_api_token}",
-
         })
 
     def get_user(self, login):
+        """Returns the user as a dictionary or None if not found"""
         url = f"{self.base_url}/api/v1/users/{login}"
         resp = self.session.get(url, timeout=TIMEOUT_SECONDS)
 
-        if resp.status_code ==  404:
+        if resp.status_code == 404:
             return None
         resp.raise_for_status()
         return resp.json()
 
+    def list_groups(self, user_id):
+        """Returns the list of groups for the user."""
+        url = f"{self.base_url}/api/v1/users/{user_id}/groups"
+        resp = self.session.get(url, timeout=TIMEOUT_SECONDS)
 
-
+        resp.raise_for_status()
+        return resp.json()
 
     
         
